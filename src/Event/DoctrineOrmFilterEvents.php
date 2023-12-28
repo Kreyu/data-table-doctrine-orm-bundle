47c924064838d5e11cbf69ec819585534c7da210
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-12-28 22:17:57 +0100

    feat: improve integration

--- src/Event/DoctrineOrmFilterEvents.php
+++ src/Event/DoctrineOrmFilterEvents.php
@@ -6,7 +6,11 @@ namespace Kreyu\Bundle\DataTableDoctrineOrmBundle\Event;
 
 final class DoctrineOrmFilterEvents
 {
+    public const PRE_SET_PARAMETERS = 'kreyu_doctrine_orm_data_table.filter.pre_set_parameters';
+
     public const PRE_APPLY_EXPRESSION = 'kreyu_doctrine_orm_data_table.filter.pre_apply_expression';
 
-    public const PRE_SET_PARAMETERS = 'kreyu_doctrine_orm_data_table.filter.pre_set_parameters';
+    private function __construct()
+    {
+    }
 }
